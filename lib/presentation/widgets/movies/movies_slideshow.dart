6aa064bc0614fe4b84028813f22a1b61f0f9b0f3
Mariano Salvetti <mariano_salvetti@gap.com>
2023-12-22 12:11:58 -0300

    carrousel de la home, estable.

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final myLocalColors = Theme.of(context).colorScheme;
     return SizedBox(
       height: 210,
       width: double.infinity, //para que ocupe todo el ancho posible
@@ -21,6 +23,15 @@ class MoviesSlideshow extends StatelessWidget {
           return _Slide(movie: movie);
         },
         itemCount: movies.length,
+        pagination: SwiperPagination(          
+          margin: const EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+            activeColor: myLocalColors.primary,
+            color: myLocalColors.secondary,
+            activeSize: 12,
+            space: 5,
+          ),
+        ),
       ),
     );
   }
@@ -34,31 +45,38 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: const FadeInImage(
-                placeholder: AssetImage('assets/no-image.jpg'),
-                image: NetworkImage("https://wowslider.com/sliders/demo-31/data1/images/bench560435.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            movie.title,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: const [
+        BoxShadow(
+          color: Colors.black45,
+          blurRadius: 10,
+          offset: Offset(0, 10),
+        ),
+      ],
     );
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30), 
+      child: DecoratedBox(
+        decoration: decoration,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child:  Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return FadeIn(child: child);
+              return const DecoratedBox(
+                decoration: BoxDecoration(color: Colors.black12),
+              );
+            }
+
+          ),
+        ),
+      ),
+      
+    );
+
   }
 }
