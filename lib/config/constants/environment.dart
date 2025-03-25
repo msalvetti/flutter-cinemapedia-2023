ac5156d733835f2766709281098a29a73385ee04
Mariano Salvetti <mariano.salvetti@globant.com>
2025-03-25 09:10:57 -0300

    actualizando del plugin de gradle. Proyecto ejecutando en Marzo 2025.-

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
 
-  static String theMovieDbKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key';
+  static String theMovieDbKey = '23850f9eb175d268fcbbaef32e2b6bf6';
 
 
 }
--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:dio/dio.dart';
 
@@ -7,6 +9,7 @@ import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:dio/io.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
   final dio = Dio(BaseOptions(
@@ -15,6 +18,17 @@ class MoviedbDatasource extends MoviesDatasource {
         'api_key': Environment.theMovieDbKey,
         'language': 'es-MX'
       }));
+
+  void checkForCharlesProxy(Dio dio) {
+    const charlesIp = '192.168.0.18';
+    print('#CharlesProxyEnabled');
+    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (client) {
+      client.findProxy = (uri) => "PROXY $charlesIp:8888;";
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+    };
+  }
 
   List<Movie> _jsonToMovies(Map<String, dynamic> json) {
     final movieDBResponse = MovieDbResponse.fromJson(json);
@@ -29,6 +43,7 @@ class MoviedbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
+ //   checkForCharlesProxy(dio);
     final response =
         await dio.get('/movie/now_playing', queryParameters: {'page': page});
     return _jsonToMovies(response.data);
